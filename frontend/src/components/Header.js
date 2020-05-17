7fd749d69504fe9dc5a062ffc7a36ded12ec42b5
Rochak Jain <rochak_r@ec.iitr.ac.in>
2020-05-17 16:31:39 +0530

    Header.js and NewStudentForm.js created

---
+++ frontend/src/components/Header.js
@@ -0,0 +1,24 @@
+import React, { Component } from "react";
+import logo from './media/logo.png';
+
+class Header extends Component {
+	render(){
+		return(
+			<div className="text-center">
+				<img
+			   		src={logo}
+			   		width="100"
+			   		className="img-thumbnail"
+			   		style={{ marginTop: "20px"}}
+				/>
+				<hr />
+				<h5>
+			   	<i>presents</i>
+				</h5>
+				<h1> KODIAC: App with React + Django </h1>
+			</div>
+		);
+	}
+}
+
+export default Header;
